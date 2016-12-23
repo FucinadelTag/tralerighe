f8580f289180ecae17f4f2b64798142b85cd90d0
Lorenzo Caldara <lorenzo@Lorenzos-MacBook-Air.local>
2016-12-23 10:42:07 +0100

    Fix yotpo

--- source/javascripts/yotpo.js
+++ source/javascripts/yotpo.js
@@ -1,1 +1,3 @@
-(function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/ZQ0ZOV9GEwJsdm4GlEGtC0d6Y4FPGcZBcFwfIjBx/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+$( document ).ready(function() {
+    (function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/ZQ0ZOV9GEwJsdm4GlEGtC0d6Y4FPGcZBcFwfIjBx/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+});
